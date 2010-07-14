26ae5b8a46d5600cf41d95e256c56d597df6563e
Michael Roberts <wftk@vivtek.com>
2010-07-14 10:03:00 +0000

    Import of MICHAEL/File-TTX-0.02 from CPAN.
    gitpan-cpan-distribution: File-TTX
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MICHAEL/File-TTX-0.02.tar.gz
    gitpan-cpan-author:       MICHAEL
    gitpan-cpan-maturity:     released

--- lib/File/TTX.pm
+++ lib/File/TTX.pm
@@ -11,11 +11,11 @@ File::TTX - Utilities for dealing with TRADOS TTX files
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
